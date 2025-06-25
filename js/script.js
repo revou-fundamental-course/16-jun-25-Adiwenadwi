656fba8d4e2b70df5b305ecc31f736bd12c0f25a
Adiwena Dwi Cahyo <216547883+Adiwenadwi@users.noreply.github.com>
2025-06-25 21:58:42 +0700

    update css

--- js/script.js
+++ js/script.js
@@ -14,23 +14,10 @@ function calcLuas(){
     console.log(alas, tinggi);
     if (alas != '' && tinggi != ''){
         const luas = (alas * tinggi) / 2;
+        const errormsg = document.getElementById('errorMsg');
+        errormsg.innerHTML = "";
         alert("Luas segitiga adalah " + luas);
     }else{
         alert("Masukkan nilai alas dan tinggi terlebih dahulu");
-    }
-}
-
-//function to calculate keliling
-//formula keliling segitiga = 3 * s
-function calcKeliling(){
-    const sisiA = document.getElementById('sisiA').value;
-    const sisiB = document.getElementById('sisiB').value;
-    const sisiC = document.getElementById('sisiC').value;
-    console.log(sisiA, sisiB, sisiC);
-    if (sisiA != '' && sisiB != '' && sisiC != ''){
-        const keliling = (sisiA + sisiB + sisiC);
-        alert("Keliling segitiga adalah " + keliling);
-    }else{
-        alert("Masukkan nilai semua sisi segitiga terlebih dahulu");
     }
 }
